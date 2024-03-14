b3baef2b27644014bf0cdf4b4a23a96233f0abee
Obie C <aikia16@uab.edu>
2024-03-14 13:29:54 -0500

    End of episode 44, The PRG pattern and Session flashing

--- demo/Http/controllers/session/store.php
+++ demo/Http/controllers/session/store.php
@@ -1,6 +1,7 @@
 <?php
 //log in the user if the credentials match.
 use Core\Authenticator;
+use Core\Session;
 use Http\Forms\LoginForm;
 
 
@@ -19,8 +20,18 @@ if ($form->validate($email, $password)) {
     $form->error('email', 'No matching account found for that email address and password.');
 }
 
-return view('session/create.view.php', [
-    'errors' => [
-        'email' => $form->errors()
-    ]
-]);
+// $session = new Session();
+// $session->flash('errors', $form->errors());
+/*
+NOTE: You do not need to create an instance of a class to call static methods.
+ Static methods belong to the class itself rather than to any specific instance 
+ of the class.
+*/
+Session::flash('errors', $form->errors());
+
+
+return redirect('/login');
+
+// return view('session/create.view.php', [
+//     'errors' => $form->errors()
+// ]);
