f8ac1b8c351c098c4b63d03241c7f075bfa402e4
Obie C <aikia16@uab.edu>
2024-03-25 08:56:05 -0500

    end of episode 47, Composer and Free Autoloading

--- demo/public/index.php
+++ demo/public/index.php
@@ -4,19 +4,21 @@ use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
+
 
 const BASE_PATH = __DIR__ . '/../';
 
 // var_dump(BASE_PATH);
+require BASE_PATH . '/vendor/autoload.php';
+
+session_start();
 
 require BASE_PATH . "Core/functions.php";
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    // dd($result);
-    require base_path("{$class}.php");
-});
+// spl_autoload_register(function ($class) {
+//     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+//     // dd($result);
+//     require base_path("{$class}.php");
+// });
 
 require base_path("bootstrap.php");
 
